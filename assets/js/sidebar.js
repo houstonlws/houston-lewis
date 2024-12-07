09fd68da98f2a4c0bd05ba74b4bff94d82487bb5
houstonlws <houstonhvrd@gmail.com>
2024-12-06 20:57:18 -0500

    genergal enhancements

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,13 +2,16 @@ document
   .getElementById('sidebar-toggle')
   .addEventListener('click', function () {
     const content = document.getElementById('sidebar-content');
+    const header = document.getElementById('sidebar-header');
     if (content.classList.contains('open')) {
       content.style.height = '0';
       content.classList.remove('open');
+      header.classList.remove('open');
     } else {
       const scrollHeight = content.scrollHeight;
       content.style.height = scrollHeight + 'px';
       content.classList.add('open');
+      header.classList.add('open');
     }
   });
 
